855d98841add86818d0bd66a03f7d6123ba95c74
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-04-26 12:10:18 +0200

    having trouble with img

--- src/app.js
+++ src/app.js
@@ -1,4 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
+  console.log(response.data.condition.icon_url);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML.setAttribute(
+    `https://api.shecodes.io/weather/v1/current?query=${response.data.condition.icon_url}&key=${apiKey}`
+  );
+
+  console.log(response.data);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+
   console.log(response.data.wind.speed);
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
