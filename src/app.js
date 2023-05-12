115b928298b9f6884fad349a5215b67a38d08a24
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-05-12 12:47:16 +0200

    new function

--- src/app.js
+++ src/app.js
@@ -22,6 +22,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return day[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -35,15 +43,23 @@ function displayForecast(response) {
       forecastHTML +
       `
        <div class="col-2">
-          <div class="weather-forecast-date">${forecastDay.time}</div>
+          <div class="weather-forecast-date">${formatDay(
+            forecastDay.time
+          )}</div>
               <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                  forecastDay.condition.icon
+                }.png"
                 alt=""
                 width="42"
                 />
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-max"> ${forecastDay.temperature.maximum}° </span>
-                <span class="weather-forecast-min"> ${forecastDay.temperature.minimum}° </span>
+                <span class="weather-forecast-max"> ${
+                  forecastDay.temperature.maximum
+                }° </span>
+                <span class="weather-forecast-min"> ${
+                  forecastDay.temperature.minimum
+                }° </span>
               </div>
        </div>
        `;
